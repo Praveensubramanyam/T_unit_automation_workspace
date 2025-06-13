e54f2d0aaaef89685fe70100b96604bebb64f554
Shailesh H <shailesh.hariharan@tigeranalytics.com>
2025-06-13 10:03:18 +0000

    ENH: Added project setup

---
+++ dynamic-pricing-tide/project_setup.py
@@ -0,0 +1,26 @@
+import os
+
+folders = [
+    "data/raw",
+    "data/processed",
+    "notebooks",
+    "src/config",
+    "src/data",
+    "src/features",
+    "src/models",
+    "src/monitor",
+    "src/utils",
+    "api/routers",
+    "frontend",
+    ".github/workflows",
+    "tests"
+]
+
+def create_folders():
+    for folder in folders:
+        os.makedirs(folder, exist_ok=True)
+        print(f"Created directory: {folder}")
+
+if __name__ == "__main__":
+    create_folders()
+    print("Folder structure initialized successfully.")
